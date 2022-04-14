12d7e2673bf64029adbf2e6f292afaeb38a9c035
vsharma2510 <vsharma2510@gmail.com>
2022-04-14 14:34:54 +0200

    Modified get_event_data.cpp to get super reduced files

--- get_event_data.cpp
+++ get_event_data.cpp
@@ -43,11 +43,11 @@ int main(int argc, char* argv[]){
 
   int dataset = 3615;
 
-  TString dir = "/project/projectdirs/cuore/syncData/CUORE/OfficialProcessed/ReproSpring20/output/";
-  TString listname;
+  TString dir = "/storage/gpfs_data/cuore/users/shared_scratch/super_reduced/";
+  TString filename_bkg,filename_cal;
   //int runs [58];
   QChain ch("qtree");
-  for(int i=0;i<58;i++)
+  /*for(int i=0;i<58;i++)
     {
       cout<<"Working on run "<<351032+i<<endl;
       for(int j=1;j<20;j++)
@@ -58,7 +58,14 @@ int main(int argc, char* argv[]){
       int nAdded = ch.Add(listname.Data());
       if(nAdded==0){cout<<"nAdded did not work"<<endl;exit(1);}
       
-    }
+      }*/
+  filename_bkg.Form("%s/SuperReduced_Background_ds%d.root",dir.Data(),dataset);
+  filename_cal.Form("%s/SuperReduced_Calibration_ds%d.root",dir.Data(),dataset);
+  int nAdded1 = ch.Add(filename_bkg.Data());
+  int nAdded2 = ch.Add(filename_cal.Data());
+
+  if(nAdded1==0 || nAdded2==0){cout<<"Could not add files"<<endl;}
+
   return 0;
 }
 
